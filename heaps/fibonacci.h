9359f9f4458b48800abfca8725b9acd9d3cb97d5
Khoa Tran <kttran2320@gmail.com>
2022-04-04 16:10:06 -0400

    added templates

---
+++ heaps/fibonacci.h
@@ -0,0 +1,33 @@
+#ifndef FIBONACCI_HEAP_H
+#define FIBONACCI_HEAP_H
+
+using namespace std;
+
+const int MAX_SIZE = 200000; //the maximum amount of elements our heap should have.
+
+template <typename Object>
+class Heap
+{
+public:
+   Heap(){
+      elements = 0;
+   };
+   void insert(Object* item){
+   };  	
+   Object* remove_min(){
+   }; 
+   
+   void decreaseKey(int pos, int val){
+   }; 
+     
+
+   bool IsEmpty() const {  return (elements <= 0);};
+   bool IsFull() const {return (elements >=MAX_SIZE );};
+   int count() const {return elements;};
+protected:
+   Object* array[MAX_SIZE];
+   int elements;       //  how many elements are in the heap
+private:
+   
+};
+#endif
