fc1bca5d86a367053a860b929eb41773f00c2755
Khoa Tran <kttran2320@gmail.com>
2022-04-28 10:41:07 -0400

    removed redundant files

--- node_utils.h
+++ node_utils.h
@@ -79,58 +79,58 @@ struct BinaryNode{
     position = -1;
   }
 };
-struct QuakeHeapNode;
-struct QuakeNode{
-      node *entry;
-      QuakeHeapNode *_top;
-      QuakeNode(node *e){
-        entry = e;
-        _top = nullptr;
-      }
-      QuakeNode(){
-        _top = nullptr;
-      }
-   };    
-struct QuakeHeapNode{
-      QuakeNode *_node;
-      unsigned int _height;
-      QuakeHeapNode *_parent;
-      QuakeHeapNode *_lchild;
-      QuakeHeapNode *_rchild;
-      std::list<QuakeHeapNode*>::iterator _root;
-      QuakeHeapNode(QuakeNode * item){
-         _node = item;
-         _height = 0;
-         _parent = nullptr;
-         _lchild = nullptr;
-         _rchild = nullptr;
-      };
-      QuakeHeapNode(QuakeHeapNode* left, QuakeHeapNode* right){
-         _lchild = left;
-         _rchild = right;
-         _parent = nullptr;
-         if(left != nullptr && right != nullptr){
-            _node = left->_node->entry->key < right->_node->entry->key ? left->_node : right->_node;
-         }
-         else if(left != nullptr){
-            _node = left->_node;
-         }
-         else if(right != nullptr){
-            _node = right->_node;
-         }
-         else{
-            std::cout << "invalid constructor call" << std::endl;
-         }
-         _height = std::max(left->_height, right->_height) + 1;
-      };
-   };    
+// struct QuakeHeapNode;
+// struct QuakeNode{
+//       node *entry;
+//       QuakeHeapNode *_top;
+//       QuakeNode(node *e){
+//         entry = e;
+//         _top = nullptr;
+//       }
+//       QuakeNode(){
+//         _top = nullptr;
+//       }
+//    };    
+// struct QuakeHeapNode{
+//       QuakeNode *_node;
+//       unsigned int _height;
+//       QuakeHeapNode *_parent;
+//       QuakeHeapNode *_lchild;
+//       QuakeHeapNode *_rchild;
+//       std::list<QuakeHeapNode*>::iterator _root;
+//       QuakeHeapNode(QuakeNode * item){
+//          _node = item;
+//          _height = 0;
+//          _parent = nullptr;
+//          _lchild = nullptr;
+//          _rchild = nullptr;
+//       };
+//       QuakeHeapNode(QuakeHeapNode* left, QuakeHeapNode* right){
+//          _lchild = left;
+//          _rchild = right;
+//          _parent = nullptr;
+//          if(left != nullptr && right != nullptr){
+//             _node = left->_node->entry->key < right->_node->entry->key ? left->_node : right->_node;
+//          }
+//          else if(left != nullptr){
+//             _node = left->_node;
+//          }
+//          else if(right != nullptr){
+//             _node = right->_node;
+//          }
+//          else{
+//             std::cout << "invalid constructor call" << std::endl;
+//          }
+//          _height = std::max(left->_height, right->_height) + 1;
+//       };
+//    };    
 
-struct rpnode{
-  node *entry;
-  int rank;
-  rpnode *next;
-  rpnode *lchild;
-  rpnode *rchild;
-};
+// struct rpnode{
+//   node *entry;
+//   int rank;
+//   rpnode *next;
+//   rpnode *lchild;
+//   rpnode *rchild;
+// };
 
 #endif
